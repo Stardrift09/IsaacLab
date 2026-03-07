28a5c2e84bf2a0a8cbdfab0fca4869aebf3c7844
Stardrift <970473702@qq.com>
2026-03-07 21:39:37 +0100

    _ rigid

--- source/isaaclab_tasks/isaaclab_tasks/direct/franka_cabinet/test_pick_it_up.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/franka_cabinet/test_pick_it_up.py
@@ -514,23 +514,23 @@ class TestPickItUp(DirectRLEnv):
             actuators={
                 "panda_shoulder": ImplicitActuatorCfg(
                     joint_names_expr=["panda_joint[1-4]"],
-                    effort_limit_sim=87.0,
-                    stiffness=80.0,
-                    damping=4.0,
+                    effort_limit_sim=300.0,
+                    stiffness=1500.0,
+                    damping=200.0,
                 ),
                 "panda_forearm": ImplicitActuatorCfg(
                     joint_names_expr=["panda_joint[5-7]"],
-                    effort_limit_sim=12.0,
-                    stiffness=80.0,
-                    damping=4.0,
+                    effort_limit_sim=150.0,
+                    stiffness=1200.0,
+                    damping=180.0,
                 ),
                 "panda_hand": ImplicitActuatorCfg(
                     joint_names_expr=["panda_finger_joint.*"],
-                    effort_limit_sim=200.0,
-                    stiffness=2e3,
-                    damping=1e2,
+                    effort_limit_sim=500.0,
+                    stiffness=8000.0,
+                    damping=400.0,
                 ),
-            },
+            }
         )
         self._robot = Articulation(robot_cfg)
         self.scene.articulations["robot"] = self._robot
